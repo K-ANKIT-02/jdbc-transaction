71f96dd11f3c9ec9af63403a9d715b99c7d773af
K-ANKIT-02 <2023546937.ankit@ug.sharda.ac.in>
2025-04-08 22:16:28 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+create database ankit;
+use ankit;
+create table accounts(
+id INT primary key,
+name varchar(50),
+balance int
+);
+insert into accounts values(1,'ram',10000);
+insert into accounts values(2,'shayam',80000);
